43c5b52f7ce1d57049df31c4987335b636573b98
GuestAariyatech <aariyatechguest@gmail.com>
2025-05-20 22:40:17 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>chetan</div>
+    </>
+  );
+}
+
+export default App;
