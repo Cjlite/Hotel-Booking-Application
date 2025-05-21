f476d636228bc7588537955bbe632b2d9f9fde19
GuestAariyatech <aariyatechguest@gmail.com>
2025-05-21 18:11:05 +0530

    1. Install react-router-dom. 2. Add image on home screen.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { Route, Routes, useLocation } from "react-router-dom";
+import Navbar from "./components/NavBar";
+import Home from "./pages/Home";
+
 function App() {
+  const isOwnerPath = useLocation().pathname.includes("owner");
+
   return (
     <>
-      <div>chetan</div>
+      <div className="">
+        {!isOwnerPath && <Navbar />}
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </div>
     </>
   );
 }
