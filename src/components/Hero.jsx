52c697523ff3896a49be3cc9ce5be2cd00bddf28
GuestAariyatech <aariyatechguest@gmail.com>
2025-05-21 18:30:39 +0530

    Add some text and style in Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,18 @@ const Hero = () => {
       className="flex flex-col items-start justify-center px-6 
     md:px-16 lg:px-24 xl:px-32 text-white bg-[url('src/assets/heroImage.png')] 
     bg-no-repeat bg-cover bg-center h-screen"
-    ></div>
+    >
+      <p className="bg-[#49B9ff]/50 px-3.5 py-1 rounded-full mt-20">
+        The Ultimate Hotel Experience
+      </p>
+      <h1 className="font-Playfair text-2xl md:text-5xl lg:text-[56px] md:leading-[56px] font-bold md:font-extrabold max-w-xl mt-4">
+        Discover Your Perfect Gateway Destination
+      </h1>
+      <p className="max-w-130 mt-2 text-sm md:text-base">
+        Unparalleled luxury and comfort await at the world's most exclusive
+        hotels and resorts. Start your journey today.
+      </p>
+    </div>
   );
 };
 
