481420abf58b9fef71b745f1419402ecc9671828
GuestAariyatech <aariyatechguest@gmail.com>
2025-05-25 17:26:09 +0530

    Make AllRooms, RoomDetails, MyBookings components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import { Route, Routes, useLocation } from "react-router-dom";
 import Navbar from "./components/NavBar";
 import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import AllRooms from "./pages/AllRooms";
+import RoomDetails from "./pages/RoomDetails";
+import MyBookings from "./pages/MyBookings";
+import HotelReg from "./components/HotelReg";
+import Layout from "./pages/HotelOwner/Layout";
+import Dashboard from "./pages/HotelOwner/Dashboard";
+import AddRoom from "./pages/HotelOwner/AddRoom";
+import ListRoom from "./pages/HotelOwner/ListRoom";
 
 function App() {
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -9,11 +18,21 @@ function App() {
     <>
       <div className="">
         {!isOwnerPath && <Navbar />}
-        <div>
+        {false && <HotelReg />}
+        <div className="min-h-[70vh]">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/rooms" element={<AllRooms />} />
+            <Route path="/rooms/:id" element={<RoomDetails />} />
+            <Route path="/my-bookings" element={<MyBookings />} />
+            <Route path="/owner" element={<Layout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="add-room" element={<AddRoom />} />
+              <Route path="list-room" element={<ListRoom />} />
+            </Route>
           </Routes>
         </div>
+        <Footer />
       </div>
     </>
   );
---
+++ src/pages/HotelOwner/Dashboard.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Title from "../../components/Title";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Title
+        align="left"
+        font="outfit"
+        title="Dashboard"
+        subTitle="Monitor your room listing, track bookings and analyze revenue-all in one place.
+        "
+      />
+      <div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
